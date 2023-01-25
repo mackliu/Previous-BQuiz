40d8ef669394b8ce658f5669ee0511d3a0c4b347
mackliu <macklun@gmail.comm>
2023-01-25 08:17:53 +0800

    11101

---
+++ 11101-bquiz03/api/get_booking.php
@@ -0,0 +1,97 @@
+<?php 
+include_once "../base.php";
+
+$ords=$Order->all(['movie'=>$_GET['movie'],'date'=>$_GET['date'],'session'=>$_GET['session']]);
+$seats=[];
+foreach($ords as $ord){
+    $s=unserialize($ord['seats']);
+    $seats=array_merge($seats,$s);
+}
+
+?>
+
+<style>
+
+#block{
+    width:540px;
+    height:370px;
+    padding-top:19px;
+    background:url("./icon/03D04.png") no-repeat center;
+    margin:auto;
+    box-sizing: border-box;
+}
+.seats{
+    width:316px;
+    height:340px;
+    margin:auto;
+    display:flex;
+    flex-wrap:wrap;
+}
+.seat{
+    width:20%;
+    height:25%;
+    text-align: center;
+    position: relative;
+}
+
+.empty{
+    background-image: url('icon/03D02.png');
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.checked{
+    background-image: url('icon/03D03.png');
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.chk{
+    position: absolute;
+    bottom:0;
+    right:0;
+}
+</style>
+<div id="block">
+    <div class='seats'>
+<?php
+for($i=0;$i<20;$i++){
+    if(!in_array($i,$seats)){
+        echo "<div class='seat empty'>";
+    }else{
+        echo "<div class='seat checked'>";
+
+    }
+    echo floor($i/5)+1;
+    echo "排";
+    echo floor($i%5)+1;
+    echo "號";
+    if(!in_array($i,$seats)){
+
+        echo "<input type='checkbox' name='seat' value='$i' class='chk'>";
+    }
+    echo "</div>";
+}
+
+?>
+    </div>
+</div>
+<div style="width:540px;margin:1rem auto;">
+    <div style="width:60%;margin:auto;">
+        <div>您選擇的電影是：<span id='movieName'></span></div>
+        <div>您選擇的時刻是：<span id='dateStr'> </span>&nbsp;&nbsp; <span id='sessionName'></span></div>
+        <div>您已經勾選<span id='tickets'></span>張票，最多可以購買四張票</div>
+        <div>
+            <button onclick="$('#order,#booking').toggle();$('#booking').html('')">上一步</button>
+            <button onclick="checkout()">訂購</button>
+        </div>
+    </div>
+</div>
+<script>
+
+function checkout(){
+    $.post("./api/order.php",info,(no)=>{
+        location.href=`?do=result&no=${no}`
+
+    })
+}
+
+</script>
